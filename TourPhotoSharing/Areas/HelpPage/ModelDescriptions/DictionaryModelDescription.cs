de70d98eea535412e345e8f6b4c63bab9c325dea
Jon Lombard <jombard@hotmail.com>
2017-08-17 00:00:05 +0100

    Add gitignores and remove temp project

--- TourPhotoSharing/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TourPhotoSharing.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- TourPhotoSharing/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TourPhotoSharing.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}

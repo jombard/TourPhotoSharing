cd4fce6115f518eb78d719c84644d7324abddad6
Jon Lombard <jombard@hotmail.com>
2017-08-17 23:59:38 +0100

    Show caption on gallery view

--- TPS.Web/Scripts/App/app.js
+++ TPS.Web/Scripts/App/app.js
@@ -4,6 +4,10 @@ $(document).on("click", ".image", function (e) {
     var $this = $(this);
     $modal.show();
     $modal.find("#modal-img").attr("src", $this.attr("src").split("?")[0]);
+
+    var caption = $this.attr("alt");
+    console.log($this, caption);
+    $modal.find(".caption").text(caption);
 });
 
 $modal.on("click", ".carousel-control", function (e) {

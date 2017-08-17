179c6b8f1965e93a91734ed5822b11ad62091c41
Jon Lombard <jombard@hotmail.com>
2017-08-17 22:33:02 +0100

    Add comment model

---
+++ TPS.Web/Core/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TPS.Web.Core.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public ApplicationUser Commenter { get; set; }
+
+        [ForeignKey("Commenter")]
+        public string CommenterId { get; set; }
+
+        public string CommentValue { get; set; }
+
+        public DateTime CommentDate { get; set; }
+    }
+}

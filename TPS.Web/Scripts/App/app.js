044b701418a21c8b18369db1a3512268fb4336c8
Jon Lombard <jombard@hotmail.com>
2017-08-19 21:07:44 +0100

    Minor adjustments

--- TPS.Web/Scripts/App/app.js
+++ TPS.Web/Scripts/App/app.js
@@ -1,33 +1,4 @@
-﻿//var $modal = $("#imageModal");
-//$(document).on("click", ".image", function (e) {
-//    e.preventDefault();
-//    var $this = $(this);
-//    $modal.show();
-//    $modal.find("#modal-img").attr("src", $this.attr("src").split("?")[0]);
-
-//    var caption = $this.attr("alt");
-//    $modal.find(".caption").text(caption);
-//});
-
-//$modal.on("click", ".carousel-control", function (e) {
-//    e.preventDefault();
-//});
-
-var pswpElement = document.querySelectorAll('.pswp')[0];
-
-// build items array
-//var items = [
-//    {
-//        src: 'https://placekitten.com/600/400',
-//        w: 600,
-//        h: 400
-//    },
-//    {
-//        src: 'https://placekitten.com/1200/900',
-//        w: 1200,
-//        h: 900
-//    }
-//];
+﻿var pswpElement = document.querySelectorAll(".pswp")[0];
 
 var items = $("img").map(function (i, pic) {
     return {
@@ -38,15 +9,10 @@ var items = $("img").map(function (i, pic) {
         author: pic.dataset.author
     }
 });
-
-// define options (if needed)
+console.log(items);
 var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
+    index: 0
 };
-
-// Initializes and opens PhotoSwipe
 
 $(document).on("click", ".image", function (e) {
     e.preventDefault();

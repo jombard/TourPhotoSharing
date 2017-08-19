93013a587f6af954ec77c6ec7b402287a72c5364
Jon Lombard <jombard@hotmail.com>
2017-08-19 21:22:48 +0100

    Add firstname and lastname inputs to register

--- TPS.Web/Controllers/AccountController.cs
+++ TPS.Web/Controllers/AccountController.cs
@@ -150,7 +150,13 @@ namespace TPS.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- TPS.Web/Scripts/App/app.js
+++ TPS.Web/Scripts/App/app.js
@@ -1,21 +1,21 @@
 ﻿var pswpElement = document.querySelectorAll(".pswp")[0];
 
-var items = $("img").map(function (i, pic) {
-    return {
-        src: pic.src.split("?")[0],
-        title: pic.alt,
-        w: 1024,
-        h: 768,
-        author: pic.dataset.author
-    }
-});
-console.log(items);
 var options = {
     index: 0
 };
 
 $(document).on("click", ".image", function (e) {
     e.preventDefault();
+
+    var items = $("img").map(function (i, pic) {
+        return {
+            src: pic.src.split("?")[0],
+            title: pic.alt,
+            w: 1024,
+            h: 768,
+            author: pic.dataset.author
+        }
+    });
 
     var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
     gallery.init();

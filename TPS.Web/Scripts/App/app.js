5f9b3d67cda5ec81167cea24277b207cebb21305
Jon Lombard <jombard@hotmail.com>
2017-08-23 13:04:42 +0100

    Open clicked on image in gallery and size accordingly

--- TPS.Web/Scripts/App/app.js
+++ TPS.Web/Scripts/App/app.js
@@ -1,21 +1,31 @@
 ﻿var pswpElement = document.querySelectorAll(".pswp")[0];
 
-var options = {
-    index: 0
-};
-
 $(document).on("click", ".image", function (e) {
     e.preventDefault();
+    var index = 0;
+    var thisImg = this;
 
     var items = $("img").map(function (i, pic) {
+        if ($(pic).hasClass("pswp__img")) {
+            return null;
+        }
+
+        if (thisImg.src.split("?")[0] === pic.src.split("?")[0]) {
+            index = i;
+        }
+
         return {
-            src: pic.src.split("?")[0],
+            src: pic.src.split("?")[0] + "?maxwidth=1024&maxheight=768&mode=crop",
             title: pic.alt,
             w: 1024,
             h: 768,
             author: pic.dataset.author
         }
     });
+
+    var options = {
+        index: index
+    };
 
     var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
     gallery.init();

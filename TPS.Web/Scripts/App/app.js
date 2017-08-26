84310f2e7a554983a8636817510ba93cc146a5ff
Jon Lombard <jombard@hotmail.com>
2017-08-26 17:31:50 +0100

    Show caption and author on hover

--- TPS.Web/Scripts/App/app.js
+++ TPS.Web/Scripts/App/app.js
@@ -1,34 +1,35 @@
-﻿var pswpElement = document.querySelectorAll(".pswp")[0];
+﻿//var pswpElement = document.querySelectorAll(".pswp")[0];
 
 $(document).on("click", ".image", function (e) {
     e.preventDefault();
-    var index = 0;
-    var thisImg = this;
 
-    var items = $("li:visible").find("img").map(function (i, pic) {
-        if ($(pic).hasClass("pswp__img")) {
-            return null;
-        }
+    //var index = 0;
+    //var thisImg = this;
 
-        if (thisImg.src.split("?")[0] === pic.src.split("?")[0]) {
-            index = i;
-        }
+    //var items = $("li:visible").find("img").map(function (i, pic) {
+    //    if ($(pic).hasClass("pswp__img")) {
+    //        return null;
+    //    }
 
-        var authorName = $(pic).data("author");
+    //    if (thisImg.src.split("?")[0] === pic.src.split("?")[0]) {
+    //        index = i;
+    //    }
 
-        return {
-            src: pic.src.split("?")[0] + "?maxwidth=1024&maxheight=768&mode=crop",
-            title: pic.alt + (authorName ? "<br />Photo by: " + authorName : ""),
-            w: 1024,
-            h: 768,
-            msrc: pic.src
-        }
-    });
+    //    var authorName = $(pic).data("author");
 
-    var options = {
-        index: index
-    };
+    //    return {
+    //        src: pic.src.split("?")[0] + "?maxwidth=1024&maxheight=768&mode=crop",
+    //        title: pic.alt + (authorName ? "<br />Photo by: " + authorName : ""),
+    //        w: 1024,
+    //        h: 768,
+    //        msrc: pic.src
+    //    }
+    //});
 
-    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-    gallery.init();
+    //var options = {
+    //    index: index
+    //};
+
+    //var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    //gallery.init();
 });

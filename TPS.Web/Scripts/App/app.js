e7ccef4c5507e2d2a70b02cfb82b386bd7dc8253
Jon Lombard <jombard@hotmail.com>
2017-08-26 20:00:49 +0100

    Reinit photoswipe gallery plugin

--- TPS.Web/Scripts/App/app.js
+++ TPS.Web/Scripts/App/app.js
@@ -1,35 +1,35 @@
-﻿//var pswpElement = document.querySelectorAll(".pswp")[0];
+﻿var pswpElement = document.querySelectorAll(".pswp")[0];
 
 $(document).on("click", ".image", function (e) {
     e.preventDefault();
+    console.log("click event on image");
+    var index = 0;
+    var thisImg = this;
 
-    //var index = 0;
-    //var thisImg = this;
+    var items = $("li:visible").find("img").map(function (i, pic) {
+        if ($(pic).hasClass("pswp__img")) {
+            return null;
+        }
 
-    //var items = $("li:visible").find("img").map(function (i, pic) {
-    //    if ($(pic).hasClass("pswp__img")) {
-    //        return null;
-    //    }
+        if (thisImg.src.split("?")[0] === pic.src.split("?")[0]) {
+            index = i;
+        }
 
-    //    if (thisImg.src.split("?")[0] === pic.src.split("?")[0]) {
-    //        index = i;
-    //    }
+        var authorName = $(pic).data("author");
 
-    //    var authorName = $(pic).data("author");
+        return {
+            src: pic.src.split("?")[0] + "?maxwidth=1024&maxheight=768&mode=crop",
+            title: pic.alt + (authorName ? "<br />Photo by: " + authorName : ""),
+            w: 1024,
+            h: 768,
+            msrc: pic.src
+        }
+    });
 
-    //    return {
-    //        src: pic.src.split("?")[0] + "?maxwidth=1024&maxheight=768&mode=crop",
-    //        title: pic.alt + (authorName ? "<br />Photo by: " + authorName : ""),
-    //        w: 1024,
-    //        h: 768,
-    //        msrc: pic.src
-    //    }
-    //});
+    var options = {
+        index: index
+    };
 
-    //var options = {
-    //    index: index
-    //};
-
-    //var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-    //gallery.init();
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
 });

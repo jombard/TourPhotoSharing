4bb0616e445f22f4ae50b76152760b4af9ba050c
Jon Lombard <jombard@hotmail.com>
2017-09-03 13:30:47 +0100

    Move scripts to own files

---
+++ TPS.Web/Scripts/App/comments.js
@@ -0,0 +1,56 @@
+﻿var tourId = 0; // TODO get tour id from page
+
+function addComment(comment) {
+    var commenter = $("<h4 />").addClass("media-heading").text(comment.Commenter.FullName);
+    var dateDiv = $("<div />").addClass("small").text("on " + moment(comment.CommentDate).format("dddd, MMMM Do YYYY, h:mm a"));
+    var p = $("<p />").addClass("lead").text(comment.CommentValue);
+
+    var mediaLeft = $("<div />").addClass("media-left").append("<a href=#><img alt='64x64' class='media-object' src='/Uploads/avatar.jpg'></a>");
+    var mediaBody = $("<div />").addClass("media-body").append(commenter).append(dateDiv).append(p);
+    var li = $("<li />").addClass("media").append(mediaLeft).append(mediaBody);
+
+    $(".media-list").append(li);
+}
+
+$(document).ready(function () {
+    $.ajax({
+        url: "/api/Comments/" + tourId,
+        method: "GET"
+    }).done(function (comments) {
+        console.log("get comments result", comments);
+
+        $.each(comments, function (i, comment) {
+            addComment(comment);
+        });
+    }).fail(function (error) {
+        console.log("get comments error", error);
+    });
+});
+
+$(".comment-entry").on("click", "button", function (e) {
+    e.preventDefault();
+
+    var commentTextarea = $(".comment-entry").find("textarea");
+
+    if (commentTextarea.val() === "") {
+        return;
+    }
+
+    var commentDto = {
+        CommentValue: commentTextarea.val(),
+        TourId: tourId
+    };
+
+    $.ajax({
+        url: "/api/Comments",
+        method: "POST",
+        data: commentDto
+    }).done(function (comment) {
+        console.log("post comment result", comment);
+        commentTextarea.val("");
+
+        addComment(comment);
+    }).fail(function (error) {
+        console.log("post comment error", error);
+    });
+});
---
+++ TPS.Web/Scripts/App/maps.js
@@ -0,0 +1,58 @@
+﻿var google = google || {};
+
+function enableMaps() {
+    $(".js-view-map").removeClass("hidden");
+}
+
+function mapInit(e) {
+    e.preventDefault();
+
+    $("#map").toggleClass("hidden");
+
+    if ($("#map").children().length)
+        return;
+
+    var mapLocations = $("img").filter(function (i, pic) {
+        return pic.dataset.lat && pic.dataset.lng && $(pic).is(":visible");
+    }).map(function (i, pic) {
+        return {
+            lat: parseFloat(pic.dataset.lat),
+            lng: parseFloat(pic.dataset.lng),
+            alt: "<div><div class='thumbnail'><img src='" + pic.src + "' /></div>" +
+            "<p>" + pic.alt + "</p><p class='small'>Photo by: " + pic.dataset.author + "</p></div>",
+            src: pic.src.split("?")[0]
+        }
+    });
+
+    if (!mapLocations.length) {
+        var noMapData = $("<div />").addClass("alert alert-danger").text("Unfortunately there is no location data for the selected photo's.");
+        $("#map").append(noMapData);
+        return;
+    }
+
+    var bounds = new google.maps.LatLngBounds();
+    var infowindow = new google.maps.InfoWindow();
+
+    var map = new google.maps.Map(document.getElementById('map'));
+    $.each(mapLocations, function (i, loc) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(loc.lat, loc.lng),
+            map: map,
+            icon: loc.src + "?w=50&h=50&mode=crop"
+        });
+        bounds.extend(marker.position);
+
+        google.maps.event.addListener(marker,
+            "click",
+            (function (marker) {
+                return function () {
+                    infowindow.setContent(loc.alt);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+    });
+
+    map.fitBounds(bounds);
+}
+
+$(".js-view-map").on("click", mapInit);

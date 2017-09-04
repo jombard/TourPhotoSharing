c0212c9e6c4ae27cc8369742cfdb6b379430f298
Jon Lombard <jombard@hotmail.com>
2017-09-04 10:57:37 +0100

    Add responsive styling to datatables

--- TPS.Web/App_Start/BundleConfig.cs
+++ TPS.Web/App_Start/BundleConfig.cs
@@ -8,35 +8,38 @@ namespace TPS.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/moment.min.js",
-                      "~/Scripts/respond.min.js"));
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/moment.min.js",
+                "~/Scripts/respond.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/dropzone").Include(
                 "~/Scripts/dropzone/dropzone.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
                 "~/Scripts/DataTables/jquery.dataTables.js",
-                "~/Scripts/DataTables/dataTables.bootstrap.js"));
+                "~/Scripts/DataTables/dataTables.bootstrap.js",
+                "~/Scripts/DataTables/dataTables.responsive.js",
+                "~/Scripts/DataTables/responsive.bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-simplex.min.css",
-                      "~/Scripts/dropzone/basic.min.css",
-                      "~/Scripts/dropzone/dropzone.min.css",
-                      "~/Content/DataTables/dataTables.bootstrap.min.css",
-                      "~/Content/default-skin/default-skin.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap-simplex.min.css",
+                "~/Scripts/dropzone/basic.min.css",
+                "~/Scripts/dropzone/dropzone.min.css",
+                "~/Content/DataTables/css/dataTables.bootstrap.min.css",
+                "~/Content/DataTables/css/responsive.bootstrap.min.css",
+                "~/Content/default-skin/default-skin.css",
+                "~/Content/site.css"));
         }
     }
 }
